76fc3b94cb9b0369c60dda9c3cf0a6cba2488880
Israel de Freitas G Dias <israelfgdias@gmail.com>
2021-02-13 23:43:16 -0300

    :construction: avançando com html, css e js

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,4 @@
+import random
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
 
@@ -6,12 +7,21 @@ from .models import Tweet
 # Create your views here.
 
 
-def home_view(requests, *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse("<h1>Hello World</h1>")
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content,
+                    "likes": random.randint(0, 1000)} for x in qs]
+    data = {
+        "response": tweets_list
+    }
+    return JsonResponse(data, status=200)
 
 
-def tweet_detai_view(requests, tweet_id, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    return render(request, "pages/home.html", context={}, status=200)
+
+
+def tweet_detai_view(request, tweet_id, *args, **kwargs):
 
     data = {
         "id": tweet_id,
